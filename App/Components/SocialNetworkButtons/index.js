fdc8ab82bdfe56809bb88f8422f3c31f11c6ef40
Sufranj Naweed <naweed@digitalcube.net>
2019-11-25 15:07:02 +0100

    integration of the landing screen

--- App/Components/SocialNetworkButtons/index.js
+++ App/Components/SocialNetworkButtons/index.js
@@ -1,17 +1,63 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { Button } from  'react-native-elements';
+import { View, TouchableNativeFeedback, TouchableHighlight } from 'react-native';
+import { Icon } from  'react-native-elements';
 
 import  Styles  from './style';
+import  Colors  from '../../Themes/Colors'
 
 export default class SocialNetwork extends Component {
     render() {
         return (
             <View style={Styles.container}>
-                <Text style={Styles.text}> textInComponent </Text>
-                <View>
+                <TouchableNativeFeedback>
+                    <Icon
+                        raised
+                        name='facebook'
+                        type='font-awesome'
+                        color={Colors.facebook}
+                        onPress={() => console.log('facebook')} 
+                    />
+                </TouchableNativeFeedback>
 
-                </View>
+                <TouchableNativeFeedback>
+                    <Icon
+                        raised
+                        name='twitter'
+                        type='font-awesome'
+                        color={Colors.twitter}
+                        onPress={() => console.log('twitter')} 
+                    />
+                </TouchableNativeFeedback>
+
+                <TouchableNativeFeedback>
+                    <Icon
+                        raised
+                        name='linkedin'
+                        type='font-awesome'
+                        color={Colors.linkedin}
+                        onPress={() => console.log('linkedin')} 
+                    />
+                </TouchableNativeFeedback>
+
+                <TouchableNativeFeedback>
+                    <Icon
+                        raised
+                        name='instagram'
+                        type='font-awesome'
+                        color={Colors.instagram}
+                        onPress={() => console.log('instagram')} 
+                    />
+                </TouchableNativeFeedback>
+
+                <TouchableNativeFeedback>
+                    <Icon
+                        raised
+                        name='youtube-play'
+                        type='font-awesome'
+                        color={Colors.youtube}
+                        onPress={() => console.log('youtube-play')} 
+                    />
+                </TouchableNativeFeedback>
             </View>
         )
     }
