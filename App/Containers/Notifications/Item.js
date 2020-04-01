740837c87114bc644ebcec87de6b5b76aa454c33
erickson <erickson@digitalcube.net>
2020-04-01 19:51:19 +0200

    inté notif

--- App/Containers/Notifications/Item.js
+++ App/Containers/Notifications/Item.js
@@ -10,32 +10,39 @@ const  Item=({item}) =>{
  
     return(
         <View style={{ 
-            flexDirection: "row", justifyContent: "center", alignItems: "center",
+            flexDirection: "row",  alignItems: "center",justifyContent:"space-around",
             paddingVertical: 25, 
             backgroundColor: item && item.seen === true ? "white" : "#F0F0F0",
             borderBottomColor: "#ECECEC",
             borderBottomWidth: 1
         }}>
             
-            <View style={{ width: 280, marginRight: 50 }}>
-                <Text style={{ flexShrink: 1, fontWeight: item && item.seen ? "normal" : "bold" }}>
+            <View style={{ width: 300, marginRight: 50,}}>
+                <Text style={{ flexShrink: 1, fontWeight: item && item.seen ? "normal" : "bold" ,paddingLeft:20}}>
                     {item.notification && item.notification.content}
                 </Text>
-                <View style={[AppStyles.style.flex, { alignItems: "center" }]}>
+                <View style={[AppStyles.style.flex, { alignItems: "center",paddingLeft:20 }]}>
                     <Icon name="access-time" size={12} containerStyle={{ marginRight: 5 }} color={"#B6B6B6"} />
                     <Text style={{color: '#B6B6B6' }}>
                         Le {item.notification && Moment(item.notification.createdAt).format("DD/MM -  H[h]mm")}
                     </Text>
                 </View>
             </View>
-            <View style={{ 
-                backgroundColor: item && item.seen !== true ? "white" : Colors.lightPrimary, 
-                width: 30, height: 30, borderRadius: 35, 
-                borderColor: item && item.seen === true ? "#979797" : Colors.lightPrimary,
-                borderWidth: 1,
-                justifyContent: "center", alignItems: "center"
-            }}>
-                <Icon name="check" color={ "white"} size={22} />
+            <View style={{flexDirection:"column",alignItems:"center",paddingRight:10 ,width:100}}>
+                <View style={{ 
+                    backgroundColor: item && item.seen !== true ? "white" : Colors.lightPrimary, 
+                    width: 30, height: 30, borderRadius: 35, 
+                    borderColor: item && item.seen === true ? "#979797" : Colors.lightPrimary,
+                    borderWidth: 1,
+                    justifyContent: "center", alignItems: "center"
+                }}>
+                
+                    <Icon name="check" color={ "white"} size={22} />
+                
+                </View>
+                {item.seen === true && (
+                    <Text style ={{fontSize:10}}>Marquer comme lu</Text>
+                )}
             </View>
         </View>
     )
